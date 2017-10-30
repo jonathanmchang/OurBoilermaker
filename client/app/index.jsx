7547059f3b5f93db97998b331b60fd0647fc37a3
Jonathan Chang <jonathanmchang@gmail.com>
2017-10-30 18:29:10 -0400

    scss

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/Main.jsx';
-import Provider from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './store';
+import '../../public/index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,14 @@ module.exports = {
           presets: ['react', 'es2015'], // if you aren't using 'babel-preset-es2015', then omit the 'es2015'
         },
       },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ],
   },
 };
